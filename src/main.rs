7a7e3cebf290ebeab90599add1d83b351f992f12
umatrix81 <umatrix81@gmail.com>
2025-08-19 16:47:17 +0300

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
+use rand::RngCore;
 use std::net::{SocketAddr, UdpSocket};
+
+const PACKET_SIZE: usize = 1536;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -12,34 +15,39 @@ struct Args {
     local_port: u16,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn parse_server_address(address: &str) -> Result<SocketAddr> {
+    address
+        .parse::<SocketAddr>()
+        .map_err(|_| format!("Неверный формат IP-адреса или порта сервера: {}", address).into())
+}
+
+fn create_socket(local_port: u16) -> Result<UdpSocket> {
+    UdpSocket::bind(format!("0.0.0.0:{}", local_port))
+        .map_err(|_| format!("Порт {} занят", local_port).into())
+}
+
+fn send_packet(socket: &UdpSocket, server_addr: SocketAddr, local_port: u16) -> Result<()> {
+    let mut packet = [0u8; PACKET_SIZE];
+    rand::rng().fill_bytes(&mut packet);
+
+    socket.send_to(&packet, server_addr)?;
+    println!(
+        "[+] UDP-пакет на {} байт отправлен с порта {}!",
+        PACKET_SIZE, local_port
+    );
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
     let args = Args::parse();
-    let server_ip_port = args.server_ip_port;
-    let local_port = args.local_port;
 
-    // Создаем UDP-сокет, привязанный к локальному порту
-    let socket = UdpSocket::bind(format!("0.0.0.0:{}", local_port));
-    match socket {
-        Ok(socket) => {
-            // Формируем адрес сервера
-            let server_addr = match server_ip_port.parse::<SocketAddr>() {
-                Ok(addr) => addr,
-                Err(_) => {
-                    println!("[-] Ошибка: неверный формат IP-адреса или порта сервера.");
-                    std::process::exit(1);
-                }
-            };
+    let server_addr = parse_server_address(&args.server_ip_port)?;
+    let socket = create_socket(args.local_port)?;
 
-            // Отправляем "магический" пакет
-            match socket.send_to(b"Fuck RKN Fuck RKN Fuck RKN Fuck RKN Fuck RKN Fuck RKN Fuck RKN Fuck RKN Fuck RKN Fuck RKN", server_addr) {
-                Ok(_) => println!("[+] UDP-пакет отправлен с порта {}!", local_port),
-                Err(e) => println!("[-] Ошибка: {}", e),
-            }
-        }
-        Err(_) => {
-            println!("Похоже, что порт {local_port} занят.");
-            std::process::exit(1);
-        }
-    }
+    send_packet(&socket, server_addr, args.local_port)?;
+
     Ok(())
 }
